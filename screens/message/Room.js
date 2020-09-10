4677de1899c3409384be525be9baa5dcd37461f4
lee <doguri1031@gmail.com>
2020-09-10 10:31:49 +0900

    pull request test

--- screens/message/Room.js
+++ screens/message/Room.js
@@ -16,41 +16,43 @@ import {
   Root,
   Text as BaseText,
 } from 'native-base';
-import {useUserInfo} from '../../AuthContext';
+import {
+  useUserInfo
+} from '../../AuthContext';
 import MessageTyper from './MessageTyper';
 import constants from '../../constants';
-const Container = styled.View`
+const Container = styled.View `
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   flex: 1;
 `;
-const MessageWrapper = styled.View`
+const MessageWrapper = styled.View `
   margin: 10px 5px;
   padding: 0px 8px;
   width: 100%;
 `;
-const MessageBox = styled.View`
+const MessageBox = styled.View `
   display: flex;
   flex-direction: column;
   align-items: center;
   height: 100%;
   width: 100%;
 `;
-const SendMessageWapper = styled.View`
+const SendMessageWapper = styled.View `
   display: flex;
   flex-direction: row-reverse;
   height: auto;
   width: 100%;
 `;
-const ReceiveMesssageWrapper = styled.View`
+const ReceiveMesssageWrapper = styled.View `
   display: flex;
   flex-direction: row;
   height: auto;
   width: 100%;
 `;
-const Message = styled.View`
+const Message = styled.View `
   display: flex;
   justify-content: center;
   padding: 10px 10px;
@@ -62,8 +64,11 @@ const Message = styled.View`
   background-color: ${(props) => props.theme.lightGreyColor};
 `;
 
-export default ({navigation, route}) => {
-  const roomId = route.params?.roomId;
+export default ({
+  navigation,
+  route
+}) => {
+  const roomId = route.params ? .roomId;
   const userInfo = useUserInfo();
   const rooms = userInfo.rooms;
   const user = userInfo.user;
@@ -73,73 +78,115 @@ export default ({navigation, route}) => {
     }
   });
   const room = selectedRoom[0];
-  return (
-    <Root>
-      <Header>
-        <Left>
-          <Icon
-            name="step-backward"
-            size={24}
-            color="white"
-            onPress={() => navigation.navigate('RoomList')}
-          />
-        </Left>
-        <Body>
-          <Title>
-            {room.participant[0].itsMe
-              ? room.participant[1].nickname
-              : room.participant[0].nickname}
-          </Title>
-          <BaseText style={{color: 'white', fontSize: 12}}>
-            {room.participant[0].itsMe
-              ? room.participant[1].location
-              : room.participant[0].location}
-          </BaseText>
-        </Body>
-        <Right />
-      </Header>
-      <Container>
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <ScrollView
-            style={{backgroundColor: '#81ecec'}}
-            contentContainerStyle={{flexDirection: 'row', flexWrap: 'wrap'}}>
-            {room.messages.map((message) => (
-              <MessageWrapper key={message.id}>
-                {message.from.itsMe ? (
-                  <SendMessageWapper>
-                    <Message>
-                      <Text
-                        style={{
-                          maxWidth: constants.width / 2.5,
-                          fontSize: 18,
-                        }}>
-                        {message.data}
-                      </Text>
-                    </Message>
-                  </SendMessageWapper>
-                ) : (
-                  <ReceiveMesssageWrapper>
-                    <Message>
-                      <Text
-                        style={{
-                          maxWidth: constants.width / 2.5,
-                          fontSize: 18,
-                        }}>
-                        {message.data}
-                      </Text>
-                    </Message>
-                  </ReceiveMesssageWrapper>
-                )}
-              </MessageWrapper>
-            ))}
-          </ScrollView>
-        </TouchableWithoutFeedback>
-        <MessageTyper
-          user={user}
-          roomId={room.id}
-          participant={room.participant}
-        />
-      </Container>
-    </Root>
+  return ( <
+    Root >
+    <
+    Header >
+    <
+    Left >
+    <
+    Icon name = "step-backward"
+    size = {
+      24
+    }
+    color = "white"
+    onPress = {
+      () => navigation.navigate('RoomList')
+    }
+    /> < /
+    Left > <
+    Body >
+    <
+    Title > {
+      room.participant[0].itsMe ?
+      room.participant[1].nickname : room.participant[0].nickname
+    } <
+    /Title> <
+    BaseText style = {
+      {
+        color: 'white',
+        fontSize: 12
+      }
+    } > {
+      room.participant[0].itsMe ?
+      room.participant[1].location : room.participant[0].location
+    } <
+    /BaseText> < /
+    Body > <
+    Right / >
+    <
+    /Header> <
+    Container >
+    <
+    TouchableWithoutFeedback onPress = {
+      Keyboard.dismiss
+    } >
+    <
+    ScrollView style = {
+      {
+        backgroundColor: '#81ecec'
+      }
+    }
+    contentContainerStyle = {
+      {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+      }
+    } > {
+      room.messages.map((message) => ( <
+        MessageWrapper key = {
+          message.id
+        } > {
+          message.from.itsMe ? ( <
+            SendMessageWapper >
+            <
+            Message >
+            <
+            Text style = {
+              {
+                maxWidth: constants.width / 2.5,
+                fontSize: 18,
+              }
+            } > {
+              message.data
+            } <
+            /Text> < /
+            Message > <
+            /SendMessageWapper>
+          ) : ( <
+            ReceiveMesssageWrapper >
+            <
+            Message >
+            <
+            Text style = {
+              {
+                maxWidth: constants.width / 2.5,
+                fontSize: 18,
+              }
+            } > {
+              message.data
+            } <
+            /Text> < /
+            Message > <
+            /ReceiveMesssageWrapper>
+          )
+        } <
+        /MessageWrapper>
+      ))
+    } <
+    /ScrollView> < /
+    TouchableWithoutFeedback > <
+    MessageTyper user = {
+      user
+    }
+    roomId = {
+      room.id
+    }
+    participant = {
+      room.participant
+    }
+    /> < /
+    Container > <
+    /Root>
   );
 };
