02ef75b6700ef1dbdadef2ac674ccb6ff018c22e
lee <doguri1031@gmail.com>
2020-10-03 11:07:31 +0900

    CreatRoom Css Fix v1

--- apolloClient.js
+++ apolloClient.js
@@ -1,4 +1,4 @@
-//import { defaults,resolvers } from './LocalState';
+// import { defaults,resolvers } from './LocalState';
 import {ApolloClient} from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {HttpLink} from 'apollo-link-http';
@@ -10,67 +10,70 @@ import {setContext} from 'apollo-link-context';
 import {getMainDefinition} from 'apollo-utilities';
 import {persistCache} from 'apollo-cache-persist';
 import {AsyncStorage} from 'react-native';
-//import { AsyncStorage } from '@react-native-community/async-storage';
+// import { AsyncStorage } from '@react-native-community/async-storage';
 
-export const APOLLO_URI = 'http://38bb2dd151a0.ngrok.io';
-export const APOLLO_URI_WS = 'ws://38bb2dd151a0.ngrok.io';
+export const APOLLO_URI = 'http://321d6683e5a9.ngrok.io';
+export const APOLLO_URI_WS = 'ws://321d6683e5a9.ngrok.io';
 
 const authLink = setContext(async (_, {headers}) => {
-  const token = await AsyncStorage.getItem('token');
+    const token = await AsyncStorage.getItem('token');
 
-  return {
-    headers: {
-      ...headers,
-      userid: token || null,
-    },
-  };
+    return {
+        headers: {
+            ...headers,
+            userid: token || null
+        }
+    };
 });
 
-export default async () => {
-  // Create an http link:
-  const httpLink = new HttpLink({
-    uri: APOLLO_URI,
-  });
+export default async () => { // Create an http link:
+    const httpLink = new HttpLink({uri: APOLLO_URI});
 
-  // Create a WebSocket link:
-  const wsLink = new WebSocketLink({
-    uri: APOLLO_URI_WS,
-    options: {
-      reconnect: true,
-      connectionParams: async () => ({
-        userid: await AsyncStorage.getItem('token'),
-      }),
-    },
-  });
+    // Create a WebSocket link:
+    const wsLink = new WebSocketLink({
+        uri: APOLLO_URI_WS,
+        options: {
+            reconnect: true,
+            connectionParams: async () => (
+                {userid: await AsyncStorage.getItem('token')}
+            )
+        }
+    });
 
-  const cache = new InMemoryCache();
-  await persistCache({
-    cache,
-    storage: AsyncStorage,
-  });
+    const cache = new InMemoryCache();
+    await persistCache({cache, storage: AsyncStorage});
 
-  const client = new ApolloClient({
-    link: ApolloLink.from([
-      onError(({graphQLErrors, networkError}) => {
-        if (graphQLErrors) graphQLErrors.forEach(({message, locations, path}) => console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`));
-        if (networkError) console.log(`[Network error]: ${networkError}`);
-      }),
-      split(
-        // split based on operation type
-        ({query}) => {
-          const definition = getMainDefinition(query);
-          return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
-        },
-        wsLink,
-        authLink.concat(httpLink),
-      ),
-      withClientState({
-        //defaults,
-        cache,
-      }),
-    ]),
-    //resolvers,
-    cache,
-  });
-  return client;
+    const client = new ApolloClient({
+        link: ApolloLink.from(
+            [
+                onError(
+                    ({graphQLErrors, networkError}) => {
+                        if (graphQLErrors) 
+                            graphQLErrors.forEach(({message, locations, path}) => console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`));
+                        
+                        if (networkError) 
+                            console.log(`[Network error]: ${networkError}`);
+                        
+                    }
+                ),
+                split(
+                    // split based on operation type
+                        ({query}) => {
+                        const definition = getMainDefinition(query);
+                        return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+                    },
+                    wsLink,
+                    authLink.concat(httpLink),
+                ),
+                withClientState(
+                    { // defaults,
+                        cache
+                    }
+                ),
+            ]
+        ),
+        // resolvers,
+        cache
+    });
+    return client;
 };
